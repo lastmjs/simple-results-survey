d0b77f0c7eaac73309a1d085a0e6464259f4e8cb
Jordan Last <jordan.michael.last@gmail.com>
2016-01-21 20:26:17 -0700

    dONE: Allow the user to control which columns are shown in the results area

--- www/components/results-area/results-area.component.ts
+++ www/components/results-area/results-area.component.ts
@@ -8,12 +8,12 @@ import 'rxjs/add/operator/map';
 	selector: 'results-area',
 	template: `
 		<div style="display: flex; flex-direction: row">
-			<div *ngFor="#title of titles" style="flex: 1">
-				<div [innerHTML]="title" style="color: grey; font-size: .75em"></div>
+			<div *ngFor="#title of rowTitles" style="flex: 1">
+				<div [innerHTML]="title.content" style="color: grey; font-size: .75em"></div>
 			</div>
 		</div>
 
-		<div *ngFor="#items of values" style="background-color: white; padding: 20px; box-shadow: 0px 0px 1px grey; margin-top: 10px; margin-bottom: 10px; cursor: pointer" (click)="rowClick(items)">
+		<div *ngFor="#items of rowValues" style="background-color: white; padding: 20px; box-shadow: 0px 0px 1px grey; margin-top: 10px; margin-bottom: 10px; cursor: pointer" (click)="rowClick(items)">
 			<div style="display: flex; flex-direction: row">
 				<div *ngFor="#value of items" [innerHTML]="value" style="flex: 1"></div>
 			</div>
@@ -23,21 +23,26 @@ import 'rxjs/add/operator/map';
 
 export class ResultsAreaComponent {
 
-	public titles;
-	public values;
+	public rowTitles;
+	public rowValues;
 
+	private allTitles;
+	private allValues;
 	private router: Router;
 
 	constructor(http: Http, prepareJson: PrepareJsonService, router: Router) {
-
 
 		this.router = router;
 
 		http.get('https://spreadsheets.google.com/feeds/cells/1MJ0qymz3GupiclvulTXgDm1hW03mN9hBCL9_vGakh98/1/public/basic?alt=json')
 			.map((res) => res.json())
 			.subscribe((data) => {
-				this.titles = prepareJson.getTitles(data.feed.entry);
-				this.values = prepareJson.getValues(data.feed.entry);
+				const rowTitlesAndValues = prepareJson.getRowTitlesAndValues(data.feed.entry);
+				this.rowTitles = rowTitlesAndValues.rowTitles;
+				this.rowValues = rowTitlesAndValues.rowValues;
+
+				this.allTitles = prepareJson.getAllTitles(data.feed.entry);
+				this.allValues = prepareJson.getAllValues(data.feed.entry);
 			});
 
 	}
--- www/services/prepare-json.service.ts
+++ www/services/prepare-json.service.ts
@@ -7,7 +7,50 @@ export class PrepareJsonService {
 
     }
 
-    getTitles(entries) {
+    getRowTitlesAndValues(entries) {
+        const rowTitles = entries.reduce((prev, curr) => {
+            //TODO substring(1) assumes the columns will always be single alpha characters A-Z, if the user has more columns this function will break
+            if (curr.title.$t.substring(1) === '1') {
+                if (/\[.*\]/.test(curr.content.$t)) {
+                    prev.push({
+                        column: curr.title.$t.substring(0, 1),
+                        content: /\[(.*)\]/.exec(curr.content.$t)[1]
+                    });
+                }
+            }
+
+            return prev;
+        }, []);
+
+        const rowValues = [];
+
+        entries.reduce((prev, curr, index) => {
+
+            if (prev.length === rowTitles.length) {
+                rowValues.push(prev);
+                prev = [];
+            }
+
+            const valueColumn = curr.title.$t.substring(0, 1);
+            const valueRow = curr.title.$t.substring(1);
+            const correspondingTitle = rowTitles.find((element) => {
+                return valueRow !== '1' && element.column === valueColumn;
+            });
+
+            if (correspondingTitle) {
+                prev.push(curr.content.$t);
+            }
+
+            return prev;
+        }, []);
+
+        return {
+            rowTitles: rowTitles,
+            rowValues: rowValues
+        };
+    }
+
+    getAllTitles(entries) {
         return entries.reduce((prev, curr) => {
             //TODO substring(1) assumes the columns will always be single alpha characters A-Z, if the user has more columns this function will break
             if (curr.title.$t.substring(1) === '1') {
@@ -20,7 +63,7 @@ export class PrepareJsonService {
     }
 
     //TODO this whole function needs to be redone functionally and immutably and maybe recursively
-    getValues(entries) {
+    getAllValues(entries) {
 
         let values = [];
 
