7a37a9cf3c4ea9d45710a7280d1cb1352806b34c
Jordan Last <jordan.michael.last@gmail.com>
2016-01-21 21:22:55 -0700

    detail page works

--- www/components/result-detail/result-detail.component.ts
+++ www/components/result-detail/result-detail.component.ts
@@ -4,9 +4,11 @@ import {RouteParams} from 'angular2/router';
 @Component({
 	selector: 'result-detail',
 	template: `
-		<div *ngFor="#item of items">
-			{{item.title}}
-			{{item.value}}
+		<div style="display: flex; flex-direction: column; background-color: white; padding: 20px; box-shadow: 0px 0px 1px grey; margin-left: 15vw; margin-right: 15vw">
+			<div *ngFor="#item of items" style="margin-top: 10px; margin-bottom: 10px;">
+				<div style="color: grey; font-size: .75em; margin-bottom: 5px">{{item.title}}</div>
+				<div>{{item.value}}</div>
+			</div>
 		</div>
     `
 })
