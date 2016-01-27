71e914608cd52e39d00a4fec61562c66110d1315
Jordan Last <jordan.michael.last@gmail.com>
2016-01-27 09:33:46 -0700

    embedding form works

--- www/components/embedded-form/embedded-form.component.ts
+++ www/components/embedded-form/embedded-form.component.ts
@@ -1,19 +1,25 @@
 import {Component} from 'angular2/core';
-import {Location} from 'angular2/router';
+import {TxtFileDataService} from '../../services/txt-file-data.service.ts';
 
 @Component({
 	selector: 'embedded-form',
 	template: `
         <div style="position: relative; height: 100vh; overflow: hidden; box-shadow: 0px 0px 5px grey">
-            <iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%" height="250" width="250" src="https://docs.google.com/forms/d/1T_6y_mC1lF25TP6tS41Dt-CElz2nl3nZyhfJLprP_ko/viewform?embedded=true#start=embed" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+            <iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%" height="250" width="250" [src]="formUrl" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
         </div>
     `
 })
 
 export class EmbeddedFormComponent {
 
-	constructor(location: Location) {
-        console.log(location.path());
+	public formUrl: string;
+
+	constructor(txtFileDataService: TxtFileDataService) {
+		txtFileDataService.loadFormUrl().subscribe((data) => {
+			if (data.trim() !== '') {
+				this.formUrl = data;
+			}
+		});
 	}
 
 }
