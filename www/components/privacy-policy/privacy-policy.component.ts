dfc51d2609ef5ff436d6c2f6764ff57fd564450e
lastmjs <jordan.michael.last@gmail.com>
2016-02-02 18:39:03 -0700

    Update privacy-policy.component.ts

--- www/components/privacy-policy/privacy-policy.component.ts
+++ www/components/privacy-policy/privacy-policy.component.ts
@@ -3,8 +3,9 @@ import {Component} from 'angular2/core';
 @Component({
 	selector: 'privacy-policy',
 	template: `
-        <p>This app collects no user information except information that may be collected by use of the Google Sheets api.</p>
-		<p>See the <a href="https://www.google.com/policies/privacy/">Google privacy policy</a> for more information.</p>
+        <p>This app collects no user information except information that may be collected by use of the Google Sheets api and from the embedded Google Form.</p>
+	<p>See the <a href="https://www.google.com/policies/privacy/">Google privacy policy</a> for more information.</p>
+	<p>All data entered into the Google Form will be displayed publicly and may be used for any lawful purpose.</p>
     `
 })
 
